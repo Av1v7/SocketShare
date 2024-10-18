bd336fed697a80c06fc318004c6c025e646e5374
Av1v <103974976+Av1v7@users.noreply.github.com>
2024-10-18 16:21:55 +0300

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,3 +1,3 @@
 class Keys:
-    SERVER_HOST = '192.168.x.x' # The IP of the computer that is running the server
+    SERVER_HOST = '192.168.x.x' # The IP of the computer that is running the server (the ip of the other computer)
     SERVER_PORT = 1999
